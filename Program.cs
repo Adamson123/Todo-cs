ab6fd3766fcefa2fa6ea7455eca8dd5cb496ca3c
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-12-31 17:21:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ namespace Todo_cs
     {
       Console.WriteLine(Utils.ChangeTextColor("Enter task index to mark incomplete:"));
       int index = Utils.HandleIndexInput();
-      Tasks.tasks[index].Completed = true;
+      Tasks.tasks[index].Completed = false;
     }
     static void DeleteTaskItem()
     {
